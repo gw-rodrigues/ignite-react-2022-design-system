f1bde121dc0096ea49990fd21a8396ef80ea1228
Gleydson Rodrigues <rodrigues.gw@gmail.com>
2023-02-02 09:20:33 +0100

    feat: add stories heading, multistep, text

---
+++ packages/docs/src/stories/Text.stories.tsx
@@ -0,0 +1,19 @@
+import type { StoryObj, Meta } from '@storybook/react'
+import { Text, TextProps } from '@ignite-ui/react'
+
+export default {
+  title: 'Typography/Text',
+  component: Text,
+  args: {
+    children:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci, reprehenderit aperiam. Sequi repellendus explicabo sit, magnam expedita a tempore dicta recusandae tempora quis repellat blanditiis, ad totam, labore possimus ratione.',
+  },
+} as Meta<TextProps>
+
+export const Primary: StoryObj<TextProps> = {}
+export const CustomTag: StoryObj<TextProps> = {
+  args: {
+    children: 'Strong text',
+    as: 'strong',
+  },
+}
